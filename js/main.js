8d8f1155d4f3259c7b39f9f0105c6d588cd90a5e
Natalja Issar <78623661+sgu6ena@users.noreply.github.com>
2021-05-13 16:09:37 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 const url = 'https://api.covid19api.com/summary';
 let $map = document.querySelector('#worldmap');
+const $ratingTable = document.querySelector('.rating-table');
 
 const Countries = [];
 
@@ -14,7 +15,6 @@ fetch(url)
         return response.json();
     })
     .then((data) => {
-        console.log('data: ', data);
 
         for (key in data.Countries) {
             if (document.getElementById('g' + data.Countries[key].CountryCode)) {
@@ -24,15 +24,45 @@ fetch(url)
                 if (data.Countries[key].NewConfirmed >= 500 && data.Countries[key].NewConfirmed < 1000) document.getElementById('g' + data.Countries[key].CountryCode).setAttribute("style", "fill: #ffebee;");
                 if (data.Countries[key].NewConfirmed > 0 && data.Countries[key].NewConfirmed < 500) document.getElementById('g' + data.Countries[key].CountryCode).setAttribute("style", "fill: #ffebee;");
                 if (data.Countries[key].NewConfirmed == 0) document.getElementById('g' + data.Countries[key].CountryCode).setAttribute("style", "fill:#128e5a;");
-            } else {
-                console.log('data.Countries[key].CountryCode: ', data.Countries[key].CountryCode, data.Countries[key].Country);
-
-                console.log('NewConfirmed: ', data.Countries[key].NewConfirmed);
-                console.log('NewDeaths: ', data.Countries[key].NewDeaths);
             }
         }
-
+        rating(data.Countries);
     });
+
+const rating = (countries) => {
+        countries.sort((a, b) => {
+            if (a.NewConfirmed > b.NewConfirmed)
+                return -1
+            else return 1
+        })
+        $ratingTable.innerHTML = '';
+        console.log('countries: ', countries);
+        for (let i = 0; i < 6; i++) {
+
+            $ratingTable.insertAdjacentHTML('beforeend', ` <div class="country ">
+            <div class="country-flag"><img src="https://www.countryflags.io/${countries[i].CountryCode}/flat/64.png" alt="flag"></div>
+            <div class="country-name">${countries[i].Country}</div>
+            <div class="country-count">${countries[i].NewConfirmed}</div>
+            <div class="country-arrow"><img src="img/arrow-down.svg" alt="" class="img-arrow"></div>
+        </div>`)
+
+        }
+    }
+    //scroll smooth - плавная прокрутка
+    {
+        const scrollLinks = document.querySelectorAll('a.scroll-link');
+
+        for (const scrollLink of scrollLinks) {
+            scrollLink.addEventListener('click', event => {
+                event.preventDefault();
+                const id = scrollLink.getAttribute('href');
+                document.querySelector(id).scrollIntoView({
+                    behavior: 'smooth', //плавная
+                    block: 'start', //до какого момента крутить
+                })
+            });
+        }
+    }
 
 //$map = map;
 console.dir(map);
