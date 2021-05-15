5299a57e19da6f54b76d5da7e5c9e615d2200fc1
Natalja Issar <78623661+sgu6ena@users.noreply.github.com>
2021-05-15 23:51:18 +0300

    Add files via upload
    add statistic

--- js/main.js
+++ js/main.js
@@ -1,74 +1,115 @@
-const url = 'https://api.covid19api.com/summary';
-let $map = document.querySelector('#worldmap');
+const COLOR1 = '622942';
+const COLOR2 = '82374f';
+const COLOR3 = 'b65164';
+const COLOR4 = 'fe7780';
+const COLOR5 = 'ffada0';
+
+// const COLOR1 = 'ff441a';
+// const COLOR2 = 'ff8903';
+// const COLOR3 = 'ffd341';
+// const COLOR4 = 'ffff87';
+// const COLOR5 = 'ffffc0';
+
+
+const urlApiCovid = 'https://api.covid19api.com/summary';
+const $map = document.querySelector('#worldmap');
 const $ratingTable = document.querySelector('.rating-table');
+const $ratingControl = document.querySelector('.rating-control');
+let page = 0;
+let maxPage = 0;
 
-const Countries = [];
-
+//создание карты
 const map = new WorldMap({
     element: 'worldmap',
     width: 800,
     height: 600,
 });
 
-fetch(url)
-    .then((response) => {
-        return response.json();
-    })
-    .then((data) => {
+//получение статистики
+const getStatistic = (url) => {
+    return fetch(url)
+        .then(response => response.json())
+        .then(data => data.Countries.sort((a, b) => a.NewConfirmed > b.NewConfirmed ? -1 : 1))
+}
 
-        for (key in data.Countries) {
-            if (document.getElementById('g' + data.Countries[key].CountryCode)) {
-                if (data.Countries[key].NewConfirmed >= 10000) document.getElementById('g' + data.Countries[key].CountryCode).setAttribute("style", "fill: #b71c1c;");
-                if (data.Countries[key].NewConfirmed >= 5000 && data.Countries[key].NewConfirmed < 10000) document.getElementById('g' + data.Countries[key].CountryCode).setAttribute("style", "fill: #e53935;");
-                if (data.Countries[key].NewConfirmed >= 1000 && data.Countries[key].NewConfirmed < 5000) document.getElementById('g' + data.Countries[key].CountryCode).setAttribute("style", "fill: #e57373;");
-                if (data.Countries[key].NewConfirmed >= 500 && data.Countries[key].NewConfirmed < 1000) document.getElementById('g' + data.Countries[key].CountryCode).setAttribute("style", "fill: #ffebee;");
-                if (data.Countries[key].NewConfirmed > 0 && data.Countries[key].NewConfirmed < 500) document.getElementById('g' + data.Countries[key].CountryCode).setAttribute("style", "fill: #ffebee;");
-                if (data.Countries[key].NewConfirmed == 0) document.getElementById('g' + data.Countries[key].CountryCode).setAttribute("style", "fill:#128e5a;");
+//раскрашивание карты в зависимости от заболевших
+const colorMap = () => {
+    getStatistic(urlApiCovid).then(data => {
+        maxPage = Math.floor(data.length / 6);
+        for (key in data) {
+            let color = '';
+            if (document.getElementById('g' + data[key].CountryCode)) {
+                switch (true) {
+                    case data[key].NewConfirmed >= 10000:
+                        color = COLOR1;
+                        break;
+                    case data[key].NewConfirmed >= 5000 && data[key].NewConfirmed < 10000:
+                        color = COLOR2;
+                        break;
+                    case data[key].NewConfirmed >= 1000 && data[key].NewConfirmed < 5000:
+                        color = COLOR3;
+                        break;
+                    case data[key].NewConfirmed >= 500 && data[key].NewConfirmed < 1000:
+                        color = COLOR4;
+                        break;
+                    case data[key].NewConfirmed > 0 && data[key].NewConfirmed < 500:
+                        color = COLOR5;
+                        break;
+                    case data[key].NewConfirmed == 0:
+                        color = "128e5a";
+                        break;
+                }
+                document.getElementById('g' + data[key].CountryCode).setAttribute("style", `fill: #${color};`);
             }
         }
-        rating(data.Countries);
     });
+}
 
-const rating = (countries) => {
-        countries.sort((a, b) => {
-            if (a.NewConfirmed > b.NewConfirmed)
-                return -1
-            else return 1
-        })
+//построение рейтинга стран
+const rating = (page) => {
+    getStatistic(urlApiCovid).then(data => {
+        maxPage = Math.floor(data.length / 6);
         $ratingTable.innerHTML = '';
-        console.log('countries: ', countries);
-        for (let i = 0; i < 6; i++) {
+        for (let i = page * 6; i < (page + 1) * 6 && i < data.length; i++) {
 
             $ratingTable.insertAdjacentHTML('beforeend', ` <div class="country ">
-            <div class="country-flag"><img src="https://www.countryflags.io/${countries[i].CountryCode}/flat/64.png" alt="flag"></div>
-            <div class="country-name">${countries[i].Country}</div>
-            <div class="country-count">${countries[i].NewConfirmed}</div>
+            <div class="country-flag"><img src="https://www.countryflags.io/${data[i].CountryCode}/flat/64.png" alt="flag"></div>
+            <div class="country-name">${data[i].Country}</div>
+            <div class="country-count">${data[i].NewConfirmed}</div>
             <div class="country-arrow"><img src="img/arrow-down.svg" alt="" class="img-arrow"></div>
-        </div>`)
-
+        </div>`);
         }
-    }
-    //scroll smooth - плавная прокрутка
-    {
-        const scrollLinks = document.querySelectorAll('a.scroll-link');
 
-        for (const scrollLink of scrollLinks) {
-            scrollLink.addEventListener('click', event => {
-                event.preventDefault();
-                const id = scrollLink.getAttribute('href');
-                document.querySelector(id).scrollIntoView({
-                    behavior: 'smooth', //плавная
-                    block: 'start', //до какого момента крутить
-                })
-            });
-        }
-    }
+    });
+}
 
-//$map = map;
-console.dir(map);
-console.log(' $map : ', $map);
-//$map.['gRU'];
-const $gru = document.querySelector('#gRU');
-console.log('gru: ', $gru);
-// $gru.setAttribute("style", "fill: red;");
-// document.getElementById('gUS').setAttribute("style", "fill: blue;");
+//события при нажатия на кнопки листания статистики
+$ratingControl.addEventListener('click', (event) => {
+
+    event.target == document.querySelector('.arrow-left') && page > 0 ? rating(--page) : ''; //стрелка "<" страница назад 
+    event.target == document.querySelector('.arrow-right') && page < maxPage ? rating(++page) : ''; //стрелка ">" страница вперед 
+    //перекрашивание стрелок при конце/начале
+    page == maxPage ? document.querySelector('.arrow-right').setAttribute('style', 'color:grey;') : document.querySelector('.arrow-left').setAttribute('style', 'color:lightgrey;');
+    page > 0 ? document.querySelector('.arrow-left').setAttribute('style', 'color:#F44A45;') : document.querySelector('.arrow-left').setAttribute('style', 'color:lightgrey;');
+})
+
+
+
+//scroll smooth - плавная прокрутка
+{
+    const scrollLinks = document.querySelectorAll('a.scroll-link');
+
+    for (const scrollLink of scrollLinks) {
+        scrollLink.addEventListener('click', event => {
+            event.preventDefault();
+            const id = scrollLink.getAttribute('href');
+            document.querySelector(id).scrollIntoView({
+                behavior: 'smooth', //плавная
+                block: 'start', //до какого момента крутить
+            })
+        });
+    }
+}
+
+rating(page);
+colorMap();
